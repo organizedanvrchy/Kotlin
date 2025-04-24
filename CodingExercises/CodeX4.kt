93ccb3c7f919366a2dcc5357deccfbd23422606b
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-04-24 16:46:03 -0400

    Update Code Exercise 4 file

--- CodingExercises/CodeX4.kt
+++ CodingExercises/CodeX4.kt
@@ -10,10 +10,40 @@ fun main() {
   println("Enter a message: ")
   var userString = readLine() ?: ""
   
-  var count = 0
+  var ct = 0
   for (char in userString){
-    count++
+    ct++
   }
 
-  println("The total length of the string is: $count")
+  println("The total length of the string is: $ct")
+  count(userString)
+}
+
+fun count(text: String) {
+
+  var char = text.toCharArray()
+  
+  var ltr = 0
+  var num = 0
+  var spc = 0
+  var otr = 0
+
+  for (ch in char.withIndex()) {
+    val c = ch.value
+    if (c.isLetter()) {
+      ltr++
+    } else if (c.isDigit()) {
+      num++
+    } else if (c.isWhitespace()) {
+      spc++
+    } else {
+      otr++
+    }
+  }
+
+  println("\nThe string contains:")
+  println("Letters: $ltr")
+  println("Numbers: $num")
+  println("Spaces: $spc")
+  println("Others: $otr")
 }
